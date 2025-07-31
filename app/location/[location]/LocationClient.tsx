b69b8eecdcdaaf13c9f9ca7e62709ae66dff3e97
Nur Muh. Briva Rizky O M <brivarizky17@gmail.com>
2025-07-31 22:17:08 +0700

    Update LocationClient.tsx

--- app/location/[location]/LocationClient.tsx
+++ app/location/[location]/LocationClient.tsx
@@ -319,7 +319,7 @@ export default function LocationClient({ decodedLocation }: { decodedLocation: s
               filteredItems.map((item) => {
                 const quantity = item.jumlah ?? 0;
                 const statusCounts = {
-                                      good: item.baik ?? 0,
+                                      good: item.baik ?? 5,
                                       broken: item.rusak ?? 0,
                                     };
 
